caf4c44db7a06ba0e843814bee2464ca7c894890
Walter Bueso <Walter Bueso>
2017-01-05 21:45:14 -0500

    removed unused using statements

--- program.cs
+++ program.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace berndt_screening_test
 {
