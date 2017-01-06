3c4848ed99f91e0191ed41fd207a4dadeebc0388
Walter Bueso <Walter Bueso>
2017-01-05 22:35:45 -0500

    added final code. Need to review and refactore

--- program.cs
+++ program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace berndt_screening_test
@@ -10,47 +11,52 @@ namespace berndt_screening_test
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\walte\Desktop\berndt-screening-test\berndt-screening-test\access.log";
-            //string csvpath = @"C:\Users\walte\Desktop\berndt-screening-test\berndt-screening-test\report.csv";
-
-            StreamReader file = new StreamReader(File.OpenRead(path));
+            StringBuilder csvContent = new StringBuilder();
+            string csvpath = @"C:\Users\walte\Desktop\sc-test-answer\report.csv";
+            
+            var filePath = File.OpenRead("access.log");
+            StreamReader file = new StreamReader(filePath);
+            
             string ex = @"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
             Regex reg = new Regex(ex);
-            int counter = 0;
-            string line;
-            string[] bline;
+
+            string fileLine;
+            string[] file_key_arr;
             var ips = new List<string>();
 
-
-            while ((line = file.ReadLine()) != null)
+            //Assuming access log file key position wont change
+            //Read each line, split spaces, and add key to file_key_arr array
+            while ((fileLine = file.ReadLine()) != null)
             {
-
-                bline = line.Split(' ');
-                /*
-                Add current ip to ips list if these conditions are met:
-                -Current line length being read is = 21
-                -element on bline[2] (client ip) matches the regular expression
-                -the current ip does not start with 207.114
-                -current port = 80
-                -current method = GET
-                */
-                if (bline.Length == 21 && reg.IsMatch(bline[2]) && !bline[2].StartsWith("207.114") && bline[7].Equals("80") && bline[8].Equals("GET"))
+                file_key_arr = fileLine.Split(' ');
+               
+               //Make sure each line in file has the key values and not just iis comments
+               //Make sure ip mathces regular expression
+               //Exclude ips with beginning with 207.112
+               //Make sure port 80
+               //make sure method = GET
+                if (file_key_arr.Length == 21 && reg.IsMatch(file_key_arr[2]) && !file_key_arr[2].StartsWith("207.114") && file_key_arr[7].Equals("80") && file_key_arr[8].Equals("GET"))
                 {
-                    ips.Add(bline[2]);
+                    //Add IP to ips List 
+                    ips.Add(file_key_arr[2]);
                 }
-                counter++;
-
             }
 
-            var s = ips.OrderByDescending(Version.Parse)
+            //Sort by most request then by highest octets
+            var sorted = ips.OrderByDescending(Version.Parse)
                  .GroupBy(x => x)
                  .Select(g => new { Value = g.Key, Count = g.Count() })
                  .OrderByDescending(x => x.Count);
-
-            foreach (var i in s)
+            
+            //Append to string builder
+            foreach (var item in sorted)
             {
-                Console.WriteLine("{0}, \"{1}\"", i.Count, i.Value);
+                Console.WriteLine("{0}, \"{1}\"", item.Count, item.Value);
+                csvContent.AppendLine(item.Count + "," + item.Value);
             }
+
+            //create new csv with report
+            File.AppendAllText(csvpath, csvContent.ToString());
 
             file.Dispose();
         }
